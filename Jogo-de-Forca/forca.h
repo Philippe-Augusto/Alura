d8633c49497b98f747240eb08a779811fcd714ec
Philippe Augusto <66576938+Philippe-Augusto@users.noreply.github.com>
2023-03-27 11:20:56 -0300

    Create forca.h

---
+++ Jogo-de-Forca/forca.h
@@ -0,0 +1,13 @@
+#define TAM_PALAVRA 20
+#include "funcoesforca.c"
+
+void abertura();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+int ganhou();
+int enforcou();
+void chutou();
+void adicionapalavra();
+int chuteserrados();
+int validaletra (char* letra);
